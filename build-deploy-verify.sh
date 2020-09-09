c61fc747205709b72d3db9af6d205b2e9c2ee82f
Maciej Próchniak <mpr@touk.pl>
2020-09-09 13:06:46 +0200

    Config refactoring, less base, start using CONFIG_FORCE

--- build-deploy-verify.sh
+++ build-deploy-verify.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 # This script is intended for local development only.
+# It should copy behaviour of 'deploy' phase in .gitlab-ci.yml
+# The RELEASE should in general be the name of branch in gitlab (sanitized by gitlab)
+# Before first usage repos should be added (as in build.script phase in .gitlab-ci.yml
 
 RELEASE="${1?usage: $(basename $0) [NAME]}"
 
 cd "$(dirname "$0")" && rm -r dist/
 
 helm package -d dist/ src/
-helm upgrade -i "${RELEASE}" dist/*.tgz --set postgresql.existingSecret="${RELEASE}-postgresql" -f deploy-values.yaml
+helm upgrade -i "${RELEASE}" dist/*.tgz --set postgresql.existingSecret="${RELEASE}-postgresql" --set telegraf.existingConfigMapName="${RELEASE}-telegraf-configuration" -f deploy-values.yaml
 
 kubectl delete jobs --all
 helm test "${RELEASE}"
