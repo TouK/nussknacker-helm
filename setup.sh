0a9c488179d76160dc167f0fd12f7743584269f5
Krzysztof Gadomski <kgd@touk.pl>
2022-03-30 13:01:33 +0200

    explicit zookeeper chart dependency

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-helm repo add touk https://helm-charts.touk.pl/public/
-helm repo add influxdata https://helm.influxdata.com/
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo add riskfocus https://riskfocus.github.io/helm-charts-public/
+helm repo add --force-update touk https://helm-charts.touk.pl/public/
+helm repo add --force-update influxdata https://helm.influxdata.com/
+helm repo add --force-update grafana https://grafana.github.io/helm-charts
+helm repo add --force-update bitnami https://charts.bitnami.com/bitnami
+helm repo add --force-update riskfocus https://riskfocus.github.io/helm-charts-public/
 helm dependencies build src
